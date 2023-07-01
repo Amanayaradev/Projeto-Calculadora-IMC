f4c14aed4133a05ce47c83c47b6fb4c90a75a269
Amanayara Slot <amanayaradev@gmail.com>
2023-07-01 15:23:51 -0300

    feat: adicionando estilos com tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-green': '#4CCC9B',
+        'dark-green': '#399660',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
+
